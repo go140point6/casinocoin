5f30e8002b8fe6f34caac2bd8b43a85138b6d33b
ajochems <andre@jochems.com>
2016-04-19 09:02:36 +0000

    Updated Linux build

--- src/compat.h
+++ src/compat.h
@@ -25,13 +25,12 @@
 #include <ifaddrs.h>
 #endif
 
-// SOCKET is defined by TDM-GCC
-// typedef u_int SOCKET;
 #ifdef WIN32
 #define MSG_NOSIGNAL        0
 #define MSG_DONTWAIT        0
 typedef int socklen_t;
 #else
+typedef u_int SOCKET;
 #include "errno.h"
 #define WSAGetLastError()   errno
 #define WSAEINVAL           EINVAL
